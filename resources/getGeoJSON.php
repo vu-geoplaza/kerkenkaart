296d43c0eae87586a7cfba978d058eedd691212f
peer35 <p.j.m.vos@vu.nl>
2020-05-06 12:45:25 +0200

    add new periode and stijl

--- resources/getGeoJSON.php
+++ resources/getGeoJSON.php
@@ -1,10 +1,7 @@
- <?php
+<?php
 
 include('db.php');
-
 $db = new db();
-
-
 
 if (isset($_POST['filter'])) {
     $data = createGeoJSON($db->getFiltered($_POST['filter']));
@@ -24,6 +21,7 @@ echo json_encode($data);
  */
 Function createGeoJSON($l)
 {
+    $stijlen = ["neogotiek", "modernisme - functionalisme", "expressionisme", "traditionalisme", "neorenaissance", "eclecticisme", "neoromaans", "classicisme", "gotiek", "renaissance", "romaans", "neoclassicisme", "neobarok", "rationalisme", "overig"];
     $geo = new stdClass();
     $geo->type = "FeatureCollection";
     $geo->crs->properties->name = 'urn:ogc:def:crs:EPSG::4326';
@@ -32,37 +30,44 @@ Function createGeoJSON($l)
     $n = 0;
 
     foreach ($l as $row) {
+        $geo->features[$n] = new stdClass();
+        $geo->features[$n]->type = "Feature";
+        $geo->features[$n]->geometry = new stdClass();
+        $geo->features[$n]->geometry->type = "Point";
+        $geo->features[$n]->geometry->coordinates[0] = (double)$row['lon'];
+        $geo->features[$n]->geometry->coordinates[1] = (double)$row['lat'];
+        $geo->features[$n]->properties = new stdClass();
+        $geo->features[$n]->properties->kerk_id = $row['id'];
+        $geo->features[$n]->properties->naam = $row['naam'];
+        if ($row['denominatie'] == 'Nederlandse Protestanten Bond') {
+            $row['denominatie'] = 'Nederlandse Protestantenbond';
+        }
+        if ($row['denominatie'] == 'Hersteld Hervormde Kerk') {
+            $row['denominatie'] = 'Overig';
+        }
+        $geo->features[$n]->properties->denominatie = $row['denominatie'];
+        $geo->features[$n]->properties->type = $row['type'];
+        if ($row['stijl'] == "classisisme") {
+            $row['stijl'] = "classicisme";
+        }
 
-
-            $geo->features[$n] = new stdClass();
-            $geo->features[$n]->type = "Feature";
-            $geo->features[$n]->geometry = new stdClass();
-            $geo->features[$n]->geometry->type = "Point";
-            $geo->features[$n]->geometry->coordinates[0] = (double)$row['lon'];
-            $geo->features[$n]->geometry->coordinates[1] = (double)$row['lat'];
-            $geo->features[$n]->properties = new stdClass();
-            $geo->features[$n]->properties->kerk_id = $row['id'];
-            $geo->features[$n]->properties->naam = $row['naam'];
-            if ($row['denominatie']=='Nederlandse Protestanten Bond'){
-                $row['denominatie']='Nederlandse Protestantenbond';
-            }
-            if ($row['denominatie']=='Hersteld Hervormde Kerk'){
-                $row['denominatie']='Overig';
-            }
-            $geo->features[$n]->properties->denominatie = $row['denominatie'];
-            $geo->features[$n]->properties->type = $row['type'];
+        if (in_array($row['stijl'], $stijlen)) {
             $geo->features[$n]->properties->stijl = $row['stijl'];
-            if ($row['huidige_bestemming']=='kerk'){
-                $hb='kerk';
-            } else {
-                $hb='anders';
-            }
-            $geo->features[$n]->properties->huidige_bestemming = $hb;
-            $geo->features[$n]->properties->plaats = $row['plaats'];
-            $geo->features[$n]->properties->monument = $row['monument'];
-            $geo->features[$n]->properties->ingebruik = $row['ingebruik'];
-            $geo->features[$n]->properties->periode = $row['periode'];
-            $n++;
+        } else {
+            $geo->features[$n]->properties->stijl = "overig";
+        }
+
+        if ($row['huidige_bestemming'] == 'kerk') {
+            $hb = 'kerk';
+        } else {
+            $hb = 'anders';
+        }
+        $geo->features[$n]->properties->huidige_bestemming = $hb;
+        $geo->features[$n]->properties->plaats = $row['plaats'];
+        $geo->features[$n]->properties->monument = $row['monument'];
+        $geo->features[$n]->properties->ingebruik = $row['ingebruik'];
+        $geo->features[$n]->properties->periode = $row['periode'];
+        $n++;
     }
     return $geo;
 }
